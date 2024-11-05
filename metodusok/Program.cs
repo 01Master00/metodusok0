614620ad5c8ccfc4542d5be50effd43649663290
01Master00 <berkibalintka@gmail.com>
2024-11-05 09:32:09 +0100

    utolsó módosítások

--- metodusok/Program.cs
+++ metodusok/Program.cs
@@ -6,8 +6,24 @@
         {
             kosonto();
             Console.WriteLine(osszeadas(18, 90));
+            int a = 5;
+            Console.WriteLine(dupla(a));
+            Console.WriteLine(a);
+            Console.WriteLine(osszeg(2, 7));
+            Console.WriteLine(osszeg(2));
+            int num1 = 4, num2 = 7;
+            csere(ref num1, ref num2);
+            Console.WriteLine(num1 + " " + num2);
+        }
+        static int n(int szam1)
+        {
+            return 0;
         }
 
+        static string n()
+        {
+            return ""
+        }
         static void kosonto()
         {
             Console.WriteLine("helló világ");
@@ -18,5 +34,21 @@
             return osszeg;  // VAGY return a+b
         }
 
+        static int dupla(int a)
+        {
+            return a * 2;
+        }
+
+        static int osszeg(int a, int b = 5)
+        {
+            return a + b;
+        }
+
+        static void csere(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
     }
 }
